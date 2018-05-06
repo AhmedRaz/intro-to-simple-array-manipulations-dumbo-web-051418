856d37d692bb79baf2b739845874118e9e659746
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 13:58:00 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,20 @@ end
 def using_unshift(array, value)
   return array.unshift(value)
 end
+
+def using_pop(array)
+  return array.pop 
+end 
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift 
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
